1d6fc7f66247d23a98d77ca8b1bcfa8cdcecd798
Julia <juli.urakova@yandex.ru>
2021-12-21 17:11:53 +0400

    Создание окон Войти и Зарегистрироваться

---
+++ login.php
@@ -0,0 +1,22 @@
+<div class="overlay"> <!— задний фон —>
+<div class="popup"> <!— окошко, которое вылазит —>
+
+<form action="" method="POST" class="log">
+<input type="text" name="login" class="auth__input login" placeholder="Логин" required>
+<input type="password" name="password" class="auth__input password" placeholder="Пароль" required>
+<button class="auth__btn">Войти</button>
+</form>
+
+
+<form action="" method="POST" class="reg">
+<input type="text" name="login" class="auth__input login" placeholder="Логин" required>
+<input type="password" name="password" class="auth__input password" placeholder="Пароль" required>
+<input type="text" name="email" class="auth__input email" placeholder="Почта" required>
+<button class="auth__btn">Зарегистрироваться</button>
+</form>
+
+</div>
+
+<span class="popup__close">&times;</span>
+
+</div>
