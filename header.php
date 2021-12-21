b461af22282b4cc133f1c20767c0d0aba9b97482
Julia <juli.urakova@yandex.ru>
2021-12-21 17:09:12 +0400

    Создание Шапки главной страницы

---
+++ header.php
@@ -0,0 +1,18 @@
+<header class="header"> <!— //шапка —>
+<div class="container">
+<div class="header-nav">
+
+<div class="logo">
+<h3>Стоматология</h3>
+</div>
+
+<div class="login">
+<div class="login-block">
+<a href="#" class="auth__open" data-auth="login">Войти</a> <!— #- заглушка, никуда не переходим-->
+<a href="#" class="auth__open" data-auth="register">Регистрация</a>
+</div>
+</div>
+
+</div>
+</div>
+</header>
