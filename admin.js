2802abb0dc1846586a4deddb9624c2f08589a301
Yulia <vjv2002@mail.ru>
2021-12-21 15:32:15 +0400

    Создание функции addEventListener - отслеживание клика

---
+++ admin.js
@@ -0,0 +1,39 @@
+// DOMContentLoaded - ждет пока вся html страница прогрузится
+document.addEventListener("DOMContentLoaded", function()
+{
+let
+doctorsSelect = document.querySelector('.doctors'),
+dateRadio = document.querySelector('.date-radio'),
+dateOne = document.querySelector('.date-block-one'),
+dateRange = document.querySelector('.date-block-range'),
+btnCheckAllNumbers = document.querySelector('.btn__all[data-action="check"]'),
+btnUncheckAllNumbers = document.querySelector('.btn__all[data-action="uncheck"]'),
+timeNumbers = document.querySelectorAll('.time'),
+submit = document.querySelector('.submit'), /*Кнопка создать*/
+isDateRange = false;
+
+
+// addEventListener - остлеживание клика
+dateRadio.addEventListener('click', function(event)
+{
+// event.target - то, на что мы кликаем
+//console.log(event.target);
+// F12 - просмотреть код, открыть консоль
+
+if (event.target.getAttribute('type') == 'radio')
+{
+if(event.target.value == 'one')
+{
+isDateRange = false;
+dateOne.style.display = 'block';
+dateRange.style.display = 'none';
+}
+else
+{
+isDateRange = true;
+dateOne.style.display = 'none';
+dateRange.style.display = 'block';
+}
+}
+});
+});
